42295fc7fe6f94eaef54adc0a574ed7f81513025
sne-ha-v <venkateshsneha050@gmail.com>
2025-03-17 04:08:37 -0400

    3.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+//import { useState, useEffect } from 'react';
+//import './App.css';
 
 /* const Person = (props) => {
   return (
@@ -21,7 +21,7 @@ const App = () => {
   );
 }  */
 
-const App = () => {
+/* const App = () => {
   const [counter, setCounter] = useState(0); //use State hook [ ] is array destructing if use is used its called hook call secound variable as same as first add set 
   
   useEffect(() => {
@@ -38,6 +38,63 @@ const App = () => {
 
     </div>
   );
-}
+} */
+
+//export default App;
+
+import React, { useState, useEffect } from "react";
+
+import MovieCard from "./MovieCard";
+import SearchIcon from "./search.svg";
+import "./App.css";
+
+const API_URL = "http://www.omdbapi.com?apikey=f2334ee8";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for movies"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
