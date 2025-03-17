f37d6b77f2dc1322aaa6b39a4ab825e3dde1620b
sne-ha-v <venkateshsneha050@gmail.com>
2025-03-17 03:14:13 -0400

    2.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
-const Person = () => {
+/* const Person = (props) => {
   return (
     <>
-    <h1>Name: John</h1>
-    <h2>Last Name: Doe</h2>
-    <h2>Age: 30</h2>
+    <h1>Name: {props.name}</h1>
+    <h2>Last Name: {props.lastName}</h2>
+    <h2>Age: {props.Age}</h2>
     </>
   )
 }
@@ -13,7 +14,28 @@ const Person = () => {
 const App = () => {
   return (
     <div className="App">
-      <Person/>
+      <Person name={'John'} lastName = {'Doe'} Age = {'25'}/>
+      <Person name={'Sneha'} lastName = {'Venkatesh'} Age = {'25'}/>
+      <Person name= "marry" lastName = 'Venkatesh' Age = {'25'}/>
+    </div>
+  );
+}  */
+
+const App = () => {
+  const [counter, setCounter] = useState(0); //use State hook [ ] is array destructing if use is used its called hook call secound variable as same as first add set 
+  
+  useEffect(() => {
+    //counter = 100;//never mutatate state maually 
+    alert("you have changed counter to " + counter)
+
+  }, [counter]);//dependency array we have set our dependency array to be empty
+
+  return (
+    <div className="App">
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+
     </div>
   );
 }
