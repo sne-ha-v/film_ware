13097f3dff9a1e84f68d0f7c2720a520c461058f
sne-ha-v <venkateshsneha050@gmail.com>
2025-03-16 23:35:21 -0400

    1.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Person = () => {
+  return (
+    <>
+    <h1>Name: John</h1>
+    <h2>Last Name: Doe</h2>
+    <h2>Age: 30</h2>
+    </>
+  )
+}
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person/>
     </div>
   );
 }
